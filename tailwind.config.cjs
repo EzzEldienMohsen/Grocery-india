a5befc057b438978f1418173d869461aad57c8e3
Ezz dghedy <ezzmohsend@gmail.com>
2024-02-12 01:22:21 +0200

    package setup

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
-/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config}'*/
 export default {
-  content: ['./src/**/*.{html,js}'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
